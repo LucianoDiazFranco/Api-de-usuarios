78023075aa871d87a0405efbf2c2825516dde5cf
LucianoDiazFranco <luciano.diaz.franco@gmail.com>
2024-08-26 10:57:56 -0300

    archivo de rutas y respuestas

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
+require('dotenv').config(); // llamamos al archivo de env donde esta el puerto
+
 module.exports = {
     app: {
-        port: process.env.PORT||4000,
+        port: process.env.PORT ||4000,
     }
 }
--- src/modulos/usuarios/rutas.js
+++ src/modulos/usuarios/rutas.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const respuesta = require('../../red/respuestas');
+
 const router = express.Router();
 
 router.get('/', function(req,res){
-    res.send('Usuarios OK')
+    respuesta.success(req, res,'Todo OK',200)
 });
 
 module.exports = router;
